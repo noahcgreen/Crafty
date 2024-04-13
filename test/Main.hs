a9f9bafebec1ac81176a85d0f39e35e353394207
Noah Green <noahc.green@icloud.com>
2024-04-12 22:17:59 -0400

    Clean up tests

--- test/Main.hs
+++ test/Main.hs
@@ -23,25 +23,28 @@ assertRight e = case e of
 parse' :: HasCallStack => String -> IO Datum
 parse' = assertRight . parse
 
+parsesTo :: HasCallStack => String -> Datum -> Assertion
+parsesTo source datum' = parse' source >>= (@?= datum')
+
 -- Tests
 
 -- Booleans
 
 testParseShortTrue :: Test
 testParseShortTrue = TestLabel "Short true" . TestCase $
-    parse' "#t" >>= (@?= Boolean True)
+    "#t" `parsesTo` Boolean True
 
 testParseShortFalse :: Test
 testParseShortFalse = TestLabel "Short false" . TestCase $
-    parse' "#f" >>= (@?= Boolean False)
+    "#f" `parsesTo` Boolean False
 
 testParseLongTrue :: Test
 testParseLongTrue = TestLabel "Long true" . TestCase $
-    parse' "#true" >>= (@?= Boolean True)
+    "#true" `parsesTo` Boolean True
 
 testParseLongFalse :: Test
 testParseLongFalse = TestLabel "Long false" . TestCase $
-    parse' "#false" >>= (@?= Boolean False)
+    "#false" `parsesTo` Boolean False
 
 booleanTests :: Test
 booleanTests = TestLabel "Booleans" $ TestList [
@@ -55,11 +58,11 @@ booleanTests = TestLabel "Booleans" $ TestList [
 
 testParseIdentifier :: Test
 testParseIdentifier = TestLabel "Identifier" . TestCase $
-    parse' "identifier" >>= (@?= Symbol "identifier")
+    "identifier" `parsesTo` Symbol "identifier"
 
 testParsePipedIdentifier :: Test
 testParsePipedIdentifier = TestLabel "Piped identifier" . TestCase $
-    parse' "|an IDENTIFIER \\x0;|" >>= (@?= Symbol "an IDENTIFIER \NUL")
+    "|an IDENTIFIER \\x0;|" `parsesTo` Symbol "an IDENTIFIER \NUL"
 
 identifierTests :: Test
 identifierTests = TestLabel "Identifiers" $ TestList [
@@ -71,19 +74,19 @@ identifierTests = TestLabel "Identifiers" $ TestList [
 
 testParseInteger :: Test
 testParseInteger = TestLabel "Parse integer" . TestCase $
-    parse' "10" >>= (@?= Number (Real (Rational (Integer 10))))
+    "10" `parsesTo` Number (Real . Rational $ Integer 10)
 
 testParseDouble :: Test
 testParseDouble = TestLabel "Parse double" . TestCase $
-    parse' "12.34" >>= (@?= Number (Real (Rational (Double 12.34))))
+    "12.34" `parsesTo` Number (Real . Rational $ Double 12.34)
 
 testParseRatio :: Test
 testParseRatio = TestLabel "Parse ratio" . TestCase $
-    parse' "1/2" >>= (@?= Number (Real (Rational (Ratio 1 2))))
+    "1/2" `parsesTo` Number (Real . Rational $ Ratio 1 2)
 
 testParseComplex :: Test
 testParseComplex = TestLabel "Parse complex" . TestCase $
-    parse' "1+2i" >>= (@?= Number (Rectangular (Rational (Integer 1)) (Rational (Integer 2))))
+    "1+2i" `parsesTo` Number (Rectangular (Rational $ Integer 1) (Rational $ Integer 2))
 
 numberTests :: Test
 numberTests = TestLabel "Numbers" $ TestList [
@@ -97,27 +100,27 @@ numberTests = TestLabel "Numbers" $ TestList [
 
 testNamedCharacters :: Test
 testNamedCharacters = TestLabel "Named characters" . TestList $ map TestCase [
-    parse' "#\\alarm" >>= (@?= Character '\7'),
-    parse' "#\\backspace" >>= (@?= Character '\8'),
-    parse' "#\\delete" >>= (@?= Character '\127'),
-    parse' "#\\escape" >>= (@?= Character '\27'),
-    parse' "#\\newline" >>= (@?= Character '\n'),
-    parse' "#\\null" >>= (@?= Character '\0'),
-    parse' "#\\return" >>= (@?= Character '\r'),
-    parse' "#\\space" >>= (@?= Character ' '),
-    parse' "#\\tab" >>= (@?= Character '\t')
+    "#\\alarm"     `parsesTo` Character '\7',
+    "#\\backspace" `parsesTo` Character '\8',
+    "#\\delete"    `parsesTo` Character '\127',
+    "#\\escape"    `parsesTo` Character '\27',
+    "#\\newline"   `parsesTo` Character '\n',
+    "#\\null"      `parsesTo` Character '\0',
+    "#\\return"    `parsesTo` Character '\r',
+    "#\\space"     `parsesTo` Character ' ',
+    "#\\tab"       `parsesTo` Character '\t'
     ]
 
 testHexCharacters :: Test
 testHexCharacters = TestLabel "Hex characters" . TestList $ map TestCase [
-    parse' "#\\x03BB" >>= (@?= Character 'λ'),
-    parse' "#\\x03bb" >>= (@?= Character 'λ')
+    "#\\x03BB" `parsesTo` Character 'λ',
+    "#\\x03bb" `parsesTo` Character 'λ'
     ]
 
 testCharacterLiterals :: Test
 testCharacterLiterals = TestLabel "Character literals" . TestList $ map TestCase [
-    parse' "#\\x" >>= (@?= Character 'x'),
-    parse' "#\\1" >>= (@?= Character '1')
+    "#\\x" `parsesTo` Character 'x',
+    "#\\1" `parsesTo` Character '1'
     ]
 
 characterTests :: Test
@@ -131,24 +134,24 @@ characterTests = TestLabel "Numbers" $ TestList [
 
 testEmptyString :: Test
 testEmptyString = TestLabel "Empty string" . TestCase $
-    parse' "\"\"" >>= (@?= String "")
+    "\"\"" `parsesTo` String ""
 
 testEscapedStringCharacters :: Test
 testEscapedStringCharacters = TestLabel "Escaped string characters" . TestList $ map TestCase [
-    parse' "\"\\a\"" >>= (@?= String "\7"),
-    parse' "\"\\b\"" >>= (@?= String "\8"),
-    parse' "\"\\t\"" >>= (@?= String "\t"),
-    parse' "\"\\n\"" >>= (@?= String "\n"),
-    parse' "\"\\r\"" >>= (@?= String "\r"),
-    parse' "\"\\\"\"" >>= (@?= String "\""),
-    parse' "\"\\\\\"" >>= (@?= String "\\"),
-    parse' "\"\\|\"" >>= (@?= String "|"),
-    parse' "\"\\x0;\"" >>= (@?= String "\NUL")
+    "\"\\a\""   `parsesTo` String "\7",
+    "\"\\b\""   `parsesTo` String "\8",
+    "\"\\t\""   `parsesTo` String "\t",
+    "\"\\n\""   `parsesTo` String "\n",
+    "\"\\r\""   `parsesTo` String "\r",
+    "\"\\\"\""  `parsesTo` String "\"",
+    "\"\\\\\""  `parsesTo` String "\\",
+    "\"\\|\""   `parsesTo` String "|",
+    "\"\\x0;\"" `parsesTo` String "\NUL"
     ]
 
 testMultiCharacterString ::Test
 testMultiCharacterString = TestLabel "Multi-character string" . TestCase $
-    parse' "\"abc123 \\|\"" >>= (@?= String "abc123 |")
+    "\"abc123 \\|\"" `parsesTo` String "abc123 |"
 
 stringTests :: Test
 stringTests = TestLabel "Strings" $ TestList [
@@ -160,7 +163,7 @@ stringTests = TestLabel "Strings" $ TestList [
 
 testEmptyVector :: Test
 testEmptyVector = TestLabel "Empty vector" . TestCase $
-    parse' "#()" >>= (@?= Vector [])
+    "#()" `parsesTo` Vector []
 
 vectorTests :: Test
 vectorTests = TestLabel "Vectors" $ TestList [
